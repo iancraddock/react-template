f852a5a6c7fc7c9a5f526d806b13531931d7624c
Ian Craddock <icraddock@idatia.com>
2021-04-09 15:28:32 +0100

    chore: removed commented code

--- src/routes/Paths.ts
+++ src/routes/Paths.ts
@@ -1,11 +1,3 @@
-// export default {
-//     routes: [
-//         { id: 1, path: '/', title: 'Home' },
-//         { id: 2, path: '/test', title: 'Test' },
-//         { id: 3, path: '/test2', title: 'Test2' },
-//     ],
-// };
-
 enum Paths {
     Home = '/',
     Test = '/test',
